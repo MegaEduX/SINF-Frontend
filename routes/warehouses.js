51f146233956a6ef64d49ac4afa40cf011ff8b57
Eduardo Almeida <me@edr.io>
2015-11-16 12:40:59 +0000

    Warehouse listing should work.

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var request = require('request');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    request('http://this-request-will-fail/', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body);
+
+            console.log("Returning " + obj + "...");
+
+            res.render('warehouses', { title: 'Warehouses', warehouses: obj });
+        } else {
+            var testObj = [
+                {"Nome": "A1", "Morada": "Rua do Zaire, 765", "Localidade": "Porto", "CodPostal": ""},
+                {"Nome": "A2", "Morada": "Rua Sousa Pinto, 1", "Localidade": "Porto", "CodPostal": ""},
+                {"Nome": "Test", "Morada": "Primavera Unreachable...", "Localidade": "...", "CodPostal": ""}
+            ];
+
+            res.render('warehouses', { title: 'Warehouses', warehouses: testObj });
+        }
+    });
 });
 
 module.exports = router;
