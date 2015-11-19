45f1467eee7797a67aa747f4b1b854610a8aa8e5
Eduardo Almeida <me@edr.io>
2015-11-19 10:12:06 +0000

    Did customers, removed routes from the navbar.

--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var request = require('request');
+
 router.get('/', function(req, res, next) {
-  res.render('customers', { title: 'Customers' });
+    request('http://this-request-will-fail/', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body);
+
+            console.log("Returning " + obj + "...");
+
+            res.render('customers', { title: 'Customers', customers: obj });
+        } else {
+            var testObj = [
+                {"Morada": "Av. dos Coqueiros", "CodCliente" : "INFORSHOW", "NomeCliente": "Inforshow, Informática Comunicação", "Moeda": "EUR", "NumContribuinte": "123456789"}
+            ];
+
+            res.render('customers', { title: 'Customers', customers: testObj });
+        }
+    });
 });
 
 module.exports = router;
