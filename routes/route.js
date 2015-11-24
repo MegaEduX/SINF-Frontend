f2833dfb7563f252ac54e5c88207693ebecca2fc
Eduardo Almeida <megaedux@me.com>
2015-11-24 18:25:52 +0000

    Started work on add to shopping cart.

--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,14 @@ var request = require('request');
 
 router.get('/', function(req, res, next) {
     console.log(req.cookies);
+
+    if (req.cookies.route) {
+        //  the route has to be calculated.
+        
+        res.render('route', { title: 'Route', route: JSON.parse(req.cookies.route) });
+    } else
+        res.render('message', { title: 'Error!', description: 'No route provided.' });
+
     res.render('route', { title: 'Route' });
 });
 
