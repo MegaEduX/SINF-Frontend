2be5137dc03eb8d03db9663881027659be8c8c05
Bruno Gonçalves <victany5@hotmail.com>
2015-11-26 22:27:08 +0000

    warehouse test

---
+++ routes/inventory.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var request = require('request');
+
+
+router.get('/:id', function(req, res, next) {
+    //  req.params.id
+
+    request(process.env.PRIMAVERA_URI + '/Warehouses/' + req.params.id, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var obj = JSON.parse(body);
+
+            console.log("Returning " + obj + "...");
+
+            res.render('inventory', { title: 'Inventory', inventory: obj });
+
+        } else {
+            var testObj = [
+                {"Artigo": "A0002", "Lote" : "LT01", "Stock": "46", "Localizacao": "A2.A.001"}
+            ];
+
+            res.render('inventory', { title: 'Inventory', inventory: testObj });
+        }
+    });
+});
+
+module.exports = router;
+
