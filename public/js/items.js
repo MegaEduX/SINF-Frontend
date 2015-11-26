8ece0aaf9c3c59fa4824370a87fbe06fd086aa43
Eduardo Almeida <me@edr.io>
2015-11-26 18:51:58 +0000

    One more.

--- public/js/items.js
+++ public/js/items.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('[name="addButton"]').each(function(index, obj) {
 		var docId = window.location.href.split("/");
 
-		docId = docId[docId.length];
+		docId = docId[docId.length - 1];
 
 		if (existsInCartCookie(docId, $(obj).attr('id')))
 			obj.hide();
