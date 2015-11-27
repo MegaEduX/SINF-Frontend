a8acd6ada9bd34c3664c9a80668dd26aa81979d4
matejbasic <mbasic0@foi.hr>
2015-11-27 16:15:34 +0000

    fixed navbar for logged in user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var checkToken = require('../api/auth/auth').checkToken;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', checkToken(), function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
