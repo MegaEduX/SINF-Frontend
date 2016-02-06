400351cdbccf4b410e27a2df694dbeb60f91810f
Eduardo Almeida <megaedux@me.com>
2016-02-06 00:28:29 +0000

    And it worked.

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var config = {
   secrets: {
     jwt: process.env.JWT || 'gumball'
   },
-  seedData: true,
+  seedData: false,
   primavera: {
   	url: (process.env.PRIMAVERA_URI ? process.env.PRIMAVERA_URI : 'http://ce6fe0db.ngrok.io/api/')
   },
