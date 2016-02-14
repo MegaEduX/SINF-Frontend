aadfdd10d9db6d3a42133f11e1e2a9083cd49c8d
matejbasic <mbasic0@foi.hr>
2016-02-14 23:22:09 +0000

    seed

--- config/config.js
+++ config/config.js
@@ -3,12 +3,13 @@ var config = {
   secrets: {
     jwt: process.env.JWT || 'gumball'
   },
-  seedData: false,
+  seedData: true,
   primavera: {
   	url: (process.env.PRIMAVERA_URI ? process.env.PRIMAVERA_URI : 'http://9fa30986.ngrok.io/api/')
   },
   db: {
     url: (process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://sinf:3cmYw6WRBZFKnv@glados.edr.io/sinf?ssl=true')
+    //url: 'mongodb://localhost/sinf'
   }
 };
 
